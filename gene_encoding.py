586ff7fa6812243fc373357ad647b34ef2793e53
dodoreddy <samrudr@gmail.com>
2022-08-11 22:14:25 +0530

    Cleaned up and added ability to output add connections and nodes

--- gene_encoding.py
+++ gene_encoding.py
@@ -1,5 +1,6 @@
 import networkx as nx
 
+
 class Node:
     def __init__(self, num, typeNode="hidden"):
         self.typeNode = typeNode
@@ -23,9 +24,11 @@ class NodeGenome:
         if self.n >= len(self.nodes):
             raise StopIteration
 
-        self.n = self.n+1
-        return self.nodes[self.n-1].num
+        self.n = self.n + 1
+        return self.nodes[self.n - 1].num
 
+    def __len__(self):
+        return len(self.nodes)
 
     def output_nodes(self):
         for i in self.nodes:
@@ -34,7 +37,7 @@ class NodeGenome:
     def add_node(self, node):
         self.nodes.append(node)
 
-    def add_node(self, nodeNum, typeNode = "hidden"):
+    def add_node(self, nodeNum, typeNode="hidden"):
         self.nodes.append(Node(nodeNum, typeNode))
 
 
@@ -51,6 +54,20 @@ class ConnectionGenome:
     def __init__(self):
         self.connections = []
 
+    def __len__(self):
+        return len(self.connections)
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n >= len(self.connections):
+            raise StopIteration
+
+        self.n = self.n + 1
+        return self.connections[self.n - 1]
+
     def add_connection(self, connection):
         self.connections.append(connection)
 
@@ -59,8 +76,8 @@ class ConnectionGenome:
 
     def output_connections(self):
         for i in self.connections:
-            print(f"Connection with innovation number {i.innov}, goes from {i.input} to {i.output} and has a weight of {i.weight}. Is enabled {i.enabled}")
-
+            print(
+                f"Connection with innovation number {i.innov}, goes from {i.input} to {i.output} and has a weight of {i.weight}. Is enabled {i.enabled}")
 
 
 class Genes(ConnectionGenome, NodeGenome):
@@ -70,7 +87,51 @@ class Genes(ConnectionGenome, NodeGenome):
         NodeGenome.__init__(self, input, output)
         ConnectionGenome.__init__(self)
 
-        for i,num in enumerate(self.nodes):
+        for i, num in enumerate(self.nodes):
             self.g.add_node(num, typeNode=self.nodes[i].typeNode)
+
+    def add_connection(self, connection):
+        if  connection.input == "output" and connection.output == "output":
+            raise Exception("input Neuron and output Neuron cannot both be output")
+
+        if connection.input == "output":
+            raise Exception("input Neuron cant be an output neuron")
+
+        if  connection.input == "input" and connection.output == "input":
+            raise Exception("input Neuron and output Neuron cannot both be input")
+
+        if connection.ouput == "input":
+            raise Exception("output Neuron cant be an input neuron")
+
+        self.g.add_edge(connection.input, connection.output, innovationNumber=connection.innov,
+                        weight=connection.weight, enabled=connection.enabled)
+
+        if not (nx.is_directed_acyclic_graph(self.g)):
+            self.g.remove_edge(connection.input, connection.output)
+            raise Exception("created a cyclic graph")
+
+        self.add_connection(connection)
+
+    def add_connection(self, input, output, weight, innov, enabled=True):
+        if input == "output" and output == "output":
+            raise Exception("input Neuron and output Neuron cannot both be output")
+
+        if connection.input == "output":
+            raise Exception("input Neuron cant be an output neuron")
+
+        if connection.input == "input" and connection.output == "input":
+            raise Exception("input Neuron and output Neuron cannot both be input")
+
+        if connection.ouput == "input":
+            raise Exception("output Neuron cant be an input neuron")
+
+        self.g.add_edge(connection.input, connection.output, innovationNumber=connection.innov,
+                        weight=connection.weight, enabled=connection.enabled)
+
+        if not (nx.is_directed_acyclic_graph(self.g)):
+            self.g.remove_edge(connection.input, connection.output)
+            raise Exception("created a cyclic graph")
+
+        self.add_connection(connection)
 
 
